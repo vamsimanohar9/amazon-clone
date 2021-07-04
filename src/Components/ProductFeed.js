38a131bd6a0673befc9f2dce91516aae019dc39b
Vamsi manohar <vamsimonohar@gmail.com>
2021-07-04 19:34:50 +0530

    added

--- src/Components/ProductFeed.js
+++ src/Components/ProductFeed.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Product from './product.js';
+import Product from './Product';
 function ProductFeed({ products }) {
     return (
         <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
